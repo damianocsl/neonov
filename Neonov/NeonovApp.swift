305182ae6e4459d8888e9be14da61be91de5a280
damianocsl <133384087+damianocsl@users.noreply.github.com>
2024-10-31 16:50:57 +0100

    Initial Commit

---
+++ Neonov/NeonovApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonovApp.swift
+//  Neonov
+//
+//  Created by Damiano on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NeonovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeonovTests/NeonovTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeonovTests.swift
+//  NeonovTests
+//
+//  Created by Damiano on 31/10/24.
+//
+
+import Testing
+@testable import Neonov
+
+struct NeonovTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
