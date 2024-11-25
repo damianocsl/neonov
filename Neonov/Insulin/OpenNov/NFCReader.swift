b9f78cd5f02f24a4310259113a19a4e39ca974ad
damianocsl <133384087+damianocsl@users.noreply.github.com>
2024-11-25 11:52:21 +0100

    update: improvement

--- Neonov/Insulin/OpenNov/NFCReader.swift
+++ Neonov/Insulin/OpenNov/NFCReader.swift
@@ -2,7 +2,7 @@
 //  NFCReader.swift
 //  Neonov
 //
-//  Created by Damiano on 31/10/24.
+//  Created by Damiano on 24/11/24.
 //
 
 import Foundation
@@ -94,20 +94,20 @@ public class NFCReader: NSObject, NFCNDEFReaderSessionDelegate {
 }
 
 //extension NFCTableViewController {
-//    
+//
 //    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
 //        let numberOfMessages = self.nfcMessages[section].count
 //        let headerTitle = numberOfMessages == 1 ? "One Message" : "\(numberOfMessages) Messages"
-//        
+//
 //        return headerTitle
 //    }
-//    
+//
 //    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //        let cell = tableView.dequeueReusableCell(withIdentifier: "NFCTableCell", for: indexPath) as! NFCTableViewCell
 //        let nfcTag = self.nfcMessages[indexPath.section][indexPath.row]
-//        
+//
 //        cell.textLabel?.text = "\(nfcTag.records.count) Records"
-//        
+//
 //        return cell
 //    }
 //}
---
+++ Neonov/Insulin/OpenNov/OpenNov.swift
@@ -0,0 +1,12 @@
+//
+//  OpenNov.swift
+//  Neonov
+//
+//  Created by Damiano on 24/11/24.
+//
+
+public class OpenNov {
+    public func processTag() -> Bool {
+        return true
+    }
+}
